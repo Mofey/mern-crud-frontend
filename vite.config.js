f890a99b63e588fb64d5f2fe7821f6b9b26a572f
mofey <mofey@outlook.com>
2024-10-21 09:38:59 +0100

    mern-crud-frontend

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/products': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  build: {
+    outDir: 'dist', // Ensure this points to the correct output directory
+  },
+});
